92174adff31e004e7dd9bd34f863e7922872be23
James Pearson Hughes <james@winnie.com>
2024-12-22 16:57:28 -0800

    Hack out most of calculate_allocations()
    Pause here because the test cases have made me realize my designed
    config file format doesn't support situations where multiple things have
    the same weight.  That is, if we have a 50/50 portfolio, 50 is the dict
    key for both funds, which looks nice but doesn't work because of the
    hash collision.  So I need to rethink what I want the config UX to look
    like.

--- rebalance.py
+++ rebalance.py
@@ -4,11 +4,20 @@
 
 import yaml
 
-def calculate_allocations(portfolio):
-    pass
+def calculate_allocations(portfolio, multiplier=1):
+    allocations = {}
+    for percentage, investment in portfolio.items():
+        if isinstance(investment, str):
+            allocations[investment] = int(percentage * multiplier)
+            continue
 
-with open('config.yaml', 'r') as f:
-    configs = yaml.safe_load(f)
+        allocations.update(calculate_allocations(investment, percentage/100))
 
-for config in configs:
-    target_allocation = calculate_allocations(config['portfolio'])
+    return allocations
+
+if __name__ == '__main__':
+    with open('config.yaml', 'r') as f:
+        configs = yaml.safe_load(f)
+
+    for config in configs:
+        target_allocation = calculate_allocations(config['portfolio'])
---
+++ test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import unittest
+from rebalance import calculate_allocations
+
+class TestCalculateAllocations(unittest.TestCase):
+    def test_empty(self):
+        pass #self.assertEqual(calculate_allocations({}), 
+
+    def test_not_100(self):
+        pass
+
+    def test_one_fund(self):
+        self.assertEqual(calculate_allocations({100: 'VFIFX'}), {'VFIFX': 100})
+
+    def test_two_fund(self):
+        self.assertEqual(calculate_allocations({60: 'VTI', 40: 'BND'}),
+                         {'VTI': 60, 'BND': 40})
+
+    def test_two_layer(self):
+        self.assertEqual(calculate_allocations(
+            {
+                60: {
+                    50: 'VT',
+                    50: 'VXUS',
+                    },
+                40: 'BND',
+            }),
+            {'VT': 30, 'VXUS': 30, 'BND': 40})
+
+if __name__ == '__main__':
+    unittest.main()
