f417b6d144630c684c5e2c2bdeadce394102fcb4
James Pearson Hughes <james@winnie.com>
2025-01-16 12:22:12 -0800

    Add benediction

--- rebalance.py
+++ rebalance.py
@@ -2,6 +2,10 @@
 # TODO: All the annoying auth stuff
 # client = ...
 
+# May you recognize your weaknesses and share your strengths.
+# May you share freely, never taking more than you give.
+# May you find love and love everyone you find.
+
 import yaml
 
 class RebalancinatorException(Exception): pass
